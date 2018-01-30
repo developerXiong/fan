46d0838dc4110390374c5de19d87b50b66335b84
fanleilei <2397707633@qq.com>
2018-01-30 10:24:42 +0800

    4
    4

---
+++ app/js/serviceArea.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(".service_li").click(function () {
+        // id = $(this).id
+        id = 7
+
+        goServiceDetails(id)
+    })
+    function goServiceDetails(id) {
+        alert(id)
+        // url = "b.html?name="+name+"&age="+age;//此处拼接内容
+        url = "serviceDetails.html?id="+id;
+        window.location.href = url
+    }
+})
--- app/js/serviceDetails.js
+++ app/js/serviceDetails.js
@@ -2,4 +2,19 @@ $(function () {
     $(".back").click(function () {
         window.history.go(-1)
     })
+    function getQueryString(name) {
+
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+
+        var r = window.location.search.substr(1).match(reg);
+
+        if (r != null){
+            return unescape(r[2]);
+        }else {
+            return null;
+        }
+    }
+    var id = getQueryString('id');
+    alert(id)
+
 })
