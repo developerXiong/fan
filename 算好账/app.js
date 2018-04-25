b5a61a35624e5ee168c0c49a0d764f18ba683395
fanleilei <2397707633@qq.com>
2018-04-25 17:34:57 +0800

    6
    6

--- 算好账/app.js
+++ 算好账/app.js
@@ -2,15 +2,26 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success:function(res){
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: '',
+            data: {
+              code: res.code
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
       }
+      
     })
     // 获取用户信息
     wx.getSetting({
--- 算好账/pages/index/index.js
+++ 算好账/pages/index/index.js
@@ -1,3 +1,5 @@
+const app = getApp()
+
 Page({
 
 
@@ -7,6 +9,7 @@ Page({
   data: {
     scrollLeft:0,
     sideShow:false,
+    userInfo:{},
     month:[
       {
         id:0,
@@ -122,6 +125,39 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //获取userinfo
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+      })
+      console.log(1)
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+        })
+      }
+      console.log(2)
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          console.log(3)
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+          })
+          console.log(this.data.userInfo)
+        },fail:res=>{console.log(res)}
+      })
+     
+    }
+   
+    console.log(options)
+
+
     this.setData({
       monthId: this.data.message.monthId,
       scrollLeft: (this.data.message.monthId)*55,
@@ -254,19 +290,19 @@ Page({
   },
 
 
-  format:function(num){
-    //格式化数字的正则
-    var num = (num || 0).toString(), temp = num.length % 3;
-    switch (temp) {
-      case 1:
-        num = '00' + num;
-        break;
-      case 2:
-        num = '0' + num;
-        break;
-    }
-    return num.match(/\d{3}/g).join(',').replace(/^0+/, '');
-  },
+  // format:function(num){
+  //   //格式化数字的正则
+  //   var num = (num || 0).toString(), temp = num.length % 3;
+  //   switch (temp) {
+  //     case 1:
+  //       num = '00' + num;
+  //       break;
+  //     case 2:
+  //       num = '0' + num;
+  //       break;
+  //   }
+  //   return num.match(/\d{3}/g).join(',').replace(/^0+/, '');
+  // },
 
 
   goPrograss:function(){
@@ -292,9 +328,10 @@ Page({
 
 
   goOrder:function(){
-    var orderNum = 'meesage.orderNum'
+    var orderNum = 'message.orderNum'
     this.setData({
       [orderNum]: 0
+    
     })
 
     wx.navigateTo({
